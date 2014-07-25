10f0a8e82e7f8fc7cf69d1d64537765ff03d5acb
Marcos CARDINOT <mcardinot@gmail.com>
2014-07-25 21:48:09 +0100

    Searching covers in the Last.fm database - initial implementation (just getting cover_urls)

--- src/dlgcoverartfetcher.cpp
+++ src/dlgcoverartfetcher.cpp
@@ -1,3 +1,4 @@
+#include <QStringBuilder>
 #include <QtDebug>
 
 #include "dlgcoverartfetcher.h"
@@ -6,6 +7,11 @@ DlgCoverArtFetcher::DlgCoverArtFetcher(QWidget *parent)
         : QDialog(parent),
           m_track(NULL) {
     setupUi(this);
+
+    connect(btnCancel, SIGNAL(clicked()),
+            this, SLOT(slotCancel()));
+    connect(btnSearch, SIGNAL(clicked()),
+            this, SLOT(slotSearch()));
 }
 
 DlgCoverArtFetcher::~DlgCoverArtFetcher() {
@@ -13,4 +19,80 @@ DlgCoverArtFetcher::~DlgCoverArtFetcher() {
 }
 
 void DlgCoverArtFetcher::init(const TrackPointer track) {
+    txtAlbum->setText(track->getAlbum());
+    txtArtist->setText(track->getArtist());
+    m_sLastRequestedAlbum.clear();
+    m_sLastRequestedArtist.clear();
+    m_searchresults.clear();
+}
+
+void DlgCoverArtFetcher::slotCancel() {
+    close();
+}
+
+void DlgCoverArtFetcher::slotSearch() {
+    btnSearch->setEnabled(false);
+    m_sLastRequestedAlbum = txtAlbum->text();
+    m_sLastRequestedArtist = txtArtist->text();
+
+    QUrl url;
+    url.setScheme("http");
+    url.setHost("ws.audioscrobbler.com");
+    url.setPath("/2.0/");
+    url.addQueryItem("method", "album.search");
+    url.addQueryItem("album", m_sLastRequestedAlbum % " " % m_sLastRequestedArtist);
+    url.addQueryItem("api_key", APIKEY_LASTFM);
+
+    QNetworkRequest req(url);
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    m_pLastReply = manager->get(req);
+    connect(m_pLastReply, SIGNAL(finished()), this, SLOT(slotSearchFinished()));
+}
+
+void DlgCoverArtFetcher::slotSearchFinished() {
+    m_searchresults.clear();
+    m_pLastReply->deleteLater();
+
+    QXmlStreamReader xml(m_pLastReply->readAll());
+    while(!xml.atEnd() && !xml.hasError())
+    {
+        xml.readNext();
+        if(xml.name() == "album") {
+            SearchResult result = parseAlbum(xml);
+            if (result.cover_url.isValid()) {
+                m_searchresults.append(result);
+            }
+        }
+    }
+    qDebug() << "found: "  << m_searchresults.size();
+    btnSearch->setEnabled(true);
+}
+
+DlgCoverArtFetcher::SearchResult DlgCoverArtFetcher::parseAlbum(
+                                                QXmlStreamReader& xml) {
+    if (xml.name() != "album") {
+        return SearchResult();
+    }
+    SearchResult result;
+    while(!xml.atEnd() && !xml.hasError())
+    {
+        xml.readNext();
+        const QStringRef &name = xml.name();
+
+        if (name == "album") {
+            break;
+        }
+
+        if (name == "name") {
+            result.album = xml.readElementText();
+        } else if (name == "artist") {
+            result.artist = xml.readElementText();
+        } else if (name == "image") {
+            if (xml.attributes().value("size").toString() == "extralarge") {
+                result.cover_url = xml.readElementText();
+            }
+        }
+    }
+
+    return result;
 }
--- src/dlgcoverartfetcher.h
+++ src/dlgcoverartfetcher.h
@@ -2,9 +2,13 @@
 #define DLGCOVERARTFETCHER_H
 
 #include <QDialog>
+#include <QNetworkReply>
+#include <QXmlStreamReader>
 
 #include "trackinfoobject.h"
 #include "ui_dlgcoverartfetcher.h"
+
+const QString APIKEY_LASTFM = "0082384c4beaad1f558827183cdc001f";
 
 class DlgCoverArtFetcher : public QDialog, public Ui::DlgCoverArtFetcher {
     Q_OBJECT
@@ -14,8 +18,25 @@ class DlgCoverArtFetcher : public QDialog, public Ui::DlgCoverArtFetcher {
 
     void init(const TrackPointer track);
 
+  private slots:
+    void slotCancel();
+    void slotSearch();
+    void slotSearchFinished();
+
   private:
+    struct SearchResult {
+      QString artist;
+      QString album;
+      QUrl cover_url;
+    };
+
     TrackPointer m_track;
+    QNetworkReply* m_pLastReply;
+    QString m_sLastRequestedAlbum;
+    QString m_sLastRequestedArtist;
+    QList<SearchResult> m_searchresults;
+
+    SearchResult parseAlbum(QXmlStreamReader& xml);
 };
 
 #endif // DLGCOVERARTFETCHER_H
